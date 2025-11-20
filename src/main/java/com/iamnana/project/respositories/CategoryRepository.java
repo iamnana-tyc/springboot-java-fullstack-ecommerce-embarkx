563e62ec9c9999555436093bdf82fbcd65b251f9
iamnana-tyc <nanatyc7@gmail.com>
2025-11-20 14:26:40 +0300

    Add category repo

---
+++ src/main/java/com/iamnana/project/respositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.iamnana.project.respositories;
+
+import com.iamnana.project.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByCategoryName(String categoryName);
+}
