5ba71b9e4f84be4ac6b33dd7f9707739bc7571df
iamnana-tyc <nanatyc7@gmail.com>
2025-11-20 14:23:26 +0300

    Implement category controller

--- src/main/java/com/iamnana/project/controller/CategoryController.java
+++ src/main/java/com/iamnana/project/controller/CategoryController.java
@@ -1,4 +1,50 @@
 package com.iamnana.project.controller;
 
+import com.iamnana.project.config.AppConstants;
+import com.iamnana.project.payload.CategoryDTO;
+import com.iamnana.project.payload.CategoryResponse;
+import com.iamnana.project.service.CategoryService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api")
 public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/public/categories")
+    public ResponseEntity<CategoryResponse> getAllCategory(
+            @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+            @RequestParam(name = "sortBy",defaultValue = AppConstants.SORT_CATEGORIES_BY, required = false) String sortBy,
+            @RequestParam(name = "sortOrder", defaultValue = AppConstants.SORT_DIRECTION, required = false) String sortOrder){
+        CategoryResponse categories = categoryService.getAllCategories(pageNumber, pageSize, sortBy, sortOrder);
+        return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
+
+    @PostMapping("/admin/categories")
+    public ResponseEntity<CategoryDTO> addCategory(@Valid @RequestBody CategoryDTO categoryDTO){
+        CategoryDTO category = categoryService.createCategory(categoryDTO);
+
+        return new ResponseEntity<>(category, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/admin/categories/{categoryId}")
+    public ResponseEntity<CategoryDTO> deleteCategory(@PathVariable Long categoryId){
+        CategoryDTO categoryDTO =  categoryService.deleteCategory(categoryId);
+        return new ResponseEntity<>(categoryDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("/admin/categories/{categoryId}")
+    public ResponseEntity<CategoryDTO> updateCategory(@Valid @RequestBody CategoryDTO categoryDTO,
+                                                 @PathVariable Long categoryId){
+        CategoryDTO savedCategory = categoryService.updateCategory(categoryDTO, categoryId);
+
+        return new ResponseEntity<>(savedCategory, HttpStatus.OK);
+    }
 }
