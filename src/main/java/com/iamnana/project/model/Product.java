33476dad4528ba4d8bb916941e0bc5cc171a59c7
iamnana-tyc <nanatyc7@gmail.com>
2025-11-29 16:32:22 +0300

    Product Object

---
+++ src/main/java/com/iamnana/project/model/Product.java
@@ -0,0 +1,35 @@
+package com.iamnana.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+
+    @NotBlank
+    @Size(min = 3, message = "Product name must be be at least 3 characters")
+    private String productName;
+
+    @NotBlank
+    @Size(min = 6, message = "Product description must be at least 6 characters")
+    private String description;
+    private String image;
+    private Integer quantity;
+    private double price;
+    private double discount;
+    private double specialPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
