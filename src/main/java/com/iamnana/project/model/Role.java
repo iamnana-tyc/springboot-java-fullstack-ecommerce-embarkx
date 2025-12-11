08c490fce0634a6515ecb7e601bdaa48f9911677
iamnana-tyc <nanatyc7@gmail.com>
2025-12-11 14:41:20 +0300

    Add role class

---
+++ src/main/java/com/iamnana/project/model/Role.java
@@ -0,0 +1,27 @@
+package com.iamnana.project.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleId;
+
+    @ToString.Exclude
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20, name = "role_name")
+    private AppRole roleName;
+
+    public Role(AppRole roleName) {
+        this.roleName = roleName;
+    }
+}
