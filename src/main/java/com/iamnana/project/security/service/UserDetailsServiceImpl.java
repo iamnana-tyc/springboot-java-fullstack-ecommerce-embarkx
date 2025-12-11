e4b66c786df1b87d5c768f1f874bd42720e6bed6
iamnana-tyc <nanatyc7@gmail.com>
2025-12-11 14:38:45 +0300

    Add custom imlementation for UserDetailsService

---
+++ src/main/java/com/iamnana/project/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.iamnana.project.security.service;
+
+import com.iamnana.project.model.User;
+import com.iamnana.project.respositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Transactional
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found for the user" + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
