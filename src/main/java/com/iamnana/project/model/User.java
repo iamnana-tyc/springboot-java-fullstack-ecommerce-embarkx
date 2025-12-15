8c59e9c39d69cc14ea0375a9f4bc8cc9b07b5441
iamnana-tyc <nanatyc7@gmail.com>
2025-12-15 13:30:10 +0300

    Fix issues in user class

--- src/main/java/com/iamnana/project/model/User.java
+++ src/main/java/com/iamnana/project/model/User.java
@@ -13,9 +13,10 @@ import java.util.Set;
 @Entity
 @Data
 @NoArgsConstructor
-@Table(name = "users", uniqueConstraints = {
-        @UniqueConstraint(columnNames = "username"),
-        @UniqueConstraint(columnNames = "email")
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
         })
 public class User {
     @Id
@@ -48,7 +49,7 @@ public class User {
     @Getter
     @Setter
     @ManyToMany(cascade = {CascadeType.PERSIST,  CascadeType.MERGE}, fetch = FetchType.EAGER)
-    @JoinTable(name = "users_roles",
+    @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
