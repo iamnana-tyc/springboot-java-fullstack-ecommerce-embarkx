992b8714739ef6b2723af11fe3098d2a6d145df2
iamnana-tyc <nanatyc7@gmail.com>
2025-12-15 13:42:01 +0300

    Add user repository

---
+++ src/main/java/com/iamnana/project/respositories/UserRepository.java
@@ -0,0 +1,20 @@
+package com.iamnana.project.respositories;
+
+import com.iamnana.project.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String username);
+
+    boolean existsByUserName(String username);
+
+    boolean existsByEmail(String email);
+
+
+}
