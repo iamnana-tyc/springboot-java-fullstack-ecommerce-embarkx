c75ddb774d5d881265727db2ea78c06e9f98799a
iamnana-tyc <nanatyc7@gmail.com>
2025-12-15 13:32:24 +0300

    Use custom user details service impl

--- src/main/java/com/iamnana/project/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/iamnana/project/security/jwt/AuthTokenFilter.java
@@ -1,6 +1,6 @@
 package com.iamnana.project.security.jwt;
 
-import com.iamnana.project.security.service.UserDetailsServiceImpl;
+import com.iamnana.project.security.services.UserDetailsServiceImpl;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
