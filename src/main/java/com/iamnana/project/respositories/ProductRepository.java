9e87437eeb1c5f50aa35c8321f99a0f836d0b8ae
iamnana-tyc <nanatyc7@gmail.com>
2025-12-15 13:33:38 +0300

    Add product repo

--- src/main/java/com/iamnana/project/respositories/ProductRepository.java
+++ src/main/java/com/iamnana/project/respositories/ProductRepository.java
@@ -6,7 +6,9 @@ import com.iamnana.project.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategoryOrderByPriceAsc(Category category, Pageable pageable);
 
