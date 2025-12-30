4ae146d12f26de47e78d22ceacc53ff4a3148552
iamnana-tyc <nanatyc7@gmail.com>
2025-12-30 11:31:20 +0300

    Implement order service

---
+++ src/main/java/com/iamnana/project/service/OrderService.java
@@ -0,0 +1,9 @@
+package com.iamnana.project.service;
+
+import com.iamnana.project.payload.OrderDTO;
+import jakarta.transaction.Transactional;
+
+public interface OrderService {
+    @Transactional
+    OrderDTO placeOrder(String emailId, Long addressId, String paymentMethod, String pgName, String pgPaymentId, String pgStatus, String pgResponseMessage);
+}
---
+++ src/main/java/com/iamnana/project/service/OrderServiceImpl.java
@@ -0,0 +1,113 @@
+package com.iamnana.project.service;
+
+import com.iamnana.project.exceptions.APIException;
+import com.iamnana.project.exceptions.ResourceNotFoundException;
+import com.iamnana.project.model.*;
+import com.iamnana.project.payload.OrderDTO;
+import com.iamnana.project.payload.OrderItemDTO;
+import com.iamnana.project.respositories.*;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    AddressRepository addressRepository;
+
+    @Autowired
+    PaymentRepository paymentRepository;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Autowired
+    OrderItemRepository orderItemRepository;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    CartService cartService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    @Transactional
+    public OrderDTO placeOrder(String emailId, Long addressId, String paymentMethod, String pgName, String pgPaymentId, String pgStatus, String pgResponseMessage) {
+        // Get the user cart
+        Cart cart = cartRepository.findCartByEmail(emailId);
+        if (cart == null){
+            throw new ResourceNotFoundException("Cart", "email",  emailId);
+        }
+
+        Address address = addressRepository.findById(addressId)
+                .orElseThrow(() -> new ResourceNotFoundException("Address", "addressId", addressId));
+
+        // Create a new order with payment info
+        Order order = new Order();
+        order.setEmail(emailId);
+        order.setAddress(address);
+        order.setOrderDate(LocalDate.now());
+        order.setTotalAmount(cart.getTotalPrice());
+        order.setOrderStatus("Order Accepted Successfully");
+
+        Payment payment = new Payment(paymentMethod, pgPaymentId, pgStatus, pgResponseMessage, pgName);
+        payment.setOrder(order);
+        payment = paymentRepository.save(payment);
+
+        order.setPayment(payment);
+        Order savedOrder = orderRepository.save(order);
+
+        // Get items from the cart into the order items
+        List<CartItem> cartItems = cart.getCartItems();
+        if (cartItems.isEmpty()){
+            throw new APIException("Cart is empty.");
+        }
+
+        List<OrderItem> orderItems = new ArrayList<>();
+        for (CartItem cartItem : cartItems){
+            OrderItem orderItem = new OrderItem();
+            orderItem.setProduct(cartItem.getProduct());
+            orderItem.setQuantity(cartItem.getQuantity());
+            orderItem.setDiscount(cartItem.getDiscount());
+            orderItem.setOrderProductPrice(cartItem.getProductPrice());
+            orderItem.setOrder(savedOrder);
+
+            orderItems.add(orderItem);
+        }
+
+        orderItems =  orderItemRepository.saveAll(orderItems);
+
+        // Update the product stock
+        cart.getCartItems().forEach(item -> {
+            int quantity = item.getQuantity();
+            Product product = item.getProduct();
+            product.setQuantity(product.getQuantity() -  quantity);
+            productRepository.save(product);
+
+            // clear the cart
+            cartService.deleteProductFromCart(cart.getCartId(), item.getProduct().getProductId());
+        });
+
+        // Send back order summary
+        OrderDTO orderDTO = modelMapper.map(savedOrder, OrderDTO.class);
+        orderItems.forEach(item -> {
+            orderDTO.getOrderItems().add(modelMapper.map(item, OrderItemDTO.class));
+        });
+
+        orderDTO.setAddressId(addressId);
+
+        return orderDTO;
+    }
+}
