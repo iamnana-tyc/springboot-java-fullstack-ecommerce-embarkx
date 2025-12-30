81a118fe19c8161ab7b9969a98908d48cc157ba2
iamnana-tyc <nanatyc7@gmail.com>
2025-12-30 11:30:19 +0300

    Add order controller

---
+++ src/main/java/com/iamnana/project/controller/OrderController.java
@@ -0,0 +1,38 @@
+package com.iamnana.project.controller;
+
+import com.iamnana.project.payload.OrderDTO;
+import com.iamnana.project.payload.OrderRequestDTO;
+import com.iamnana.project.service.OrderService;
+import com.iamnana.project.util.AuthUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private AuthUtil authUtil;
+
+    @PostMapping("/order/users/payments/{paymentMethod}")
+    public ResponseEntity<OrderDTO> orderProducts(@PathVariable String paymentMethod,
+                                                @RequestBody OrderRequestDTO orderRequestDTO){
+        String emailId = authUtil.loggedInEmail();
+        OrderDTO order =  orderService.placeOrder(
+                emailId,
+                orderRequestDTO.getAddressId(),
+                paymentMethod,
+                orderRequestDTO.getPgName(),
+                orderRequestDTO.getPgPaymentId(),
+                orderRequestDTO.getPgStatus(),
+                orderRequestDTO.getPgResponseMessage()
+        );
+
+        return new ResponseEntity<>(order, HttpStatus.CREATED);
+    }
+}
