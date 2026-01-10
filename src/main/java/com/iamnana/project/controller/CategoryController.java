0e85c3247943dc7f264f8bb5b02d65654d6e4fbb
iamnana-tyc <nanatyc7@gmail.com>
2026-01-10 23:18:13 +0300

    Document category controller

--- src/main/java/com/iamnana/project/controller/CategoryController.java
+++ src/main/java/com/iamnana/project/controller/CategoryController.java
@@ -4,19 +4,26 @@ import com.iamnana.project.config.AppConstants;
 import com.iamnana.project.payload.CategoryDTO;
 import com.iamnana.project.payload.CategoryResponse;
 import com.iamnana.project.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+@Tag(name = "Category", description = "APIs for managing category endpoints")
 @RestController
 @RequestMapping("/api")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @Operation(summary = "Get categories", description = "API to get all categories")
     @GetMapping("/public/categories")
     public ResponseEntity<CategoryResponse> getAllCategory(
             @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
@@ -27,6 +34,13 @@ public class CategoryController {
         return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 
+
+    @Operation(summary = "Create category",  description = "API to create a new category" )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Category created successfully."),
+            @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content),
+    })
     @PostMapping("/admin/categories")
     public ResponseEntity<CategoryDTO> addCategory(@Valid @RequestBody CategoryDTO categoryDTO){
         CategoryDTO category = categoryService.createCategory(categoryDTO);
@@ -34,15 +48,26 @@ public class CategoryController {
         return new ResponseEntity<>(category, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Delete category", description = "API to delete an existing category")
     @DeleteMapping("/admin/categories/{categoryId}")
-    public ResponseEntity<CategoryDTO> deleteCategory(@PathVariable Long categoryId){
+    public ResponseEntity<CategoryDTO> deleteCategory(
+            @Parameter(description = "The Id of the category you want to delete")
+            @PathVariable Long categoryId){
         CategoryDTO categoryDTO =  categoryService.deleteCategory(categoryId);
         return new ResponseEntity<>(categoryDTO, HttpStatus.OK);
     }
 
+    @Operation(summary = "Update category", description = "API to update an existing category")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Category created successfully."),
+            @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content),
+    })
     @PutMapping("/admin/categories/{categoryId}")
-    public ResponseEntity<CategoryDTO> updateCategory(@Valid @RequestBody CategoryDTO categoryDTO,
-                                                 @PathVariable Long categoryId){
+    public ResponseEntity<CategoryDTO> updateCategory(
+            @Valid @RequestBody CategoryDTO categoryDTO,
+            @Parameter(description = "The id of category you want to update.")
+            @PathVariable Long categoryId){
         CategoryDTO savedCategory = categoryService.updateCategory(categoryDTO, categoryId);
 
         return new ResponseEntity<>(savedCategory, HttpStatus.OK);
