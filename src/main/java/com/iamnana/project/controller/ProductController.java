4acb4b1c6103dc779f738d2d0d7390038dd9e69d
iamnana-tyc <nanatyc7@gmail.com>
2026-01-10 23:24:54 +0300

    Document product controller

--- src/main/java/com/iamnana/project/controller/ProductController.java
+++ src/main/java/com/iamnana/project/controller/ProductController.java
@@ -4,6 +4,12 @@ import com.iamnana.project.config.AppConstants;
 import com.iamnana.project.payload.ProductDTO;
 import com.iamnana.project.payload.ProductResponse;
 import com.iamnana.project.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,21 +17,31 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.swing.plaf.SeparatorUI;
 import java.io.IOException;
 
+@Tag(name = "Product", description = "APIs for managing product endpoints")
 @RestController
 @RequestMapping("/api")
 public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @Operation(summary = "Add product", description = "API to add a product")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Product created successfully."),
+            @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content),
+    })
     @PostMapping("/admin/categories/{categoryId}/products")
     public ResponseEntity<ProductDTO> addProduct(@Valid @RequestBody ProductDTO productDTO,
+                                                 @Parameter(description = "The id of the category")
                                                  @PathVariable Long categoryId){
         ProductDTO savedProductDTO = productService.saveProduct(productDTO, categoryId);
         return new ResponseEntity<>(savedProductDTO, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Get all products", description = "API to get all products")
     @GetMapping("/public/products")
     public ResponseEntity<ProductResponse> getAllProducts(
             @RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
@@ -38,6 +54,7 @@ public class ProductController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    @Operation(summary = "Get product by category", description = "API to get products by category")
     @GetMapping("/public/categories/{categoryId}/products")
     public ResponseEntity<ProductResponse> getProductByCategory(
             @PathVariable Long categoryId,
@@ -52,6 +69,7 @@ public class ProductController {
 
     }
 
+    @Operation(summary = "Get product", description = "API endpoint to get product by keyword")
     @GetMapping("/public/products/keyword/{keyword}")
     public ResponseEntity<ProductResponse> getProductsByKeyword(
             @PathVariable String keyword,
@@ -65,23 +83,37 @@ public class ProductController {
         return new ResponseEntity<>(response, HttpStatus.FOUND);
     }
 
+    @Operation(summary = "Update product", description = "API endpoint to update product")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Product updated successfully."),
+            @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content),
+    })
     @PutMapping("/admin/products/{productId}")
-    public ResponseEntity<ProductDTO> updateProduct(@Valid @PathVariable Long productId,
-                                                    @RequestBody ProductDTO productDTO){
+    public ResponseEntity<ProductDTO> updateProduct(
+            @Parameter(description = "The id of the product")
+            @Valid @PathVariable Long productId,
+            @RequestBody ProductDTO productDTO){
         ProductDTO updatedProduct = productService.updateProduct(productId, productDTO);
 
         return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete product", description = "API endpoint to delete product")
     @DeleteMapping("/admin/products/{productId}")
-    public ResponseEntity<String> deleteProduct(@PathVariable Long productId){
+    public ResponseEntity<String> deleteProduct(
+            @Parameter(description = "The id of the product")
+            @PathVariable Long productId){
         productService.deleteProduct(productId);
         return new ResponseEntity<>("Product successfully deleted", HttpStatus.OK);
     }
 
+    @Operation(summary = "Update product image", description = "API endpoint to update product image")
     @PutMapping("/products/{productId}/image")
-    public ResponseEntity<ProductDTO> updateProductImage(@PathVariable Long productId,
-                                                         @RequestParam("image")MultipartFile image) throws IOException {
+    public ResponseEntity<ProductDTO> updateProductImage(
+            @Parameter(description = "The id of the product")
+            @PathVariable Long productId,
+            @RequestParam("image")MultipartFile image) throws IOException {
         ProductDTO updateProductDTO = productService.updateProductImage(productId,image);
 
         return new ResponseEntity<>(updateProductDTO, HttpStatus.OK);
