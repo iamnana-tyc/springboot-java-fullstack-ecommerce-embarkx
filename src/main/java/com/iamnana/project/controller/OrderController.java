71e2f1c2c947ba043e6202a80e281b3552be4131
iamnana-tyc <nanatyc7@gmail.com>
2026-01-10 23:19:44 +0300

    Document order controller

--- src/main/java/com/iamnana/project/controller/OrderController.java
+++ src/main/java/com/iamnana/project/controller/OrderController.java
@@ -4,11 +4,17 @@ import com.iamnana.project.payload.OrderDTO;
 import com.iamnana.project.payload.OrderRequestDTO;
 import com.iamnana.project.service.OrderService;
 import com.iamnana.project.util.AuthUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Order", description = "APIs for managing the order endpoints")
 @RestController
 @RequestMapping("/api")
 public class OrderController {
@@ -19,6 +25,12 @@ public class OrderController {
     @Autowired
     private AuthUtil authUtil;
 
+    @Operation(summary = "Order product", description = "API endpoint for placing order")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Order created successfully."),
+            @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content),
+    })
     @PostMapping("/order/users/payments/{paymentMethod}")
     public ResponseEntity<OrderDTO> orderProducts(@PathVariable String paymentMethod,
                                                 @RequestBody OrderRequestDTO orderRequestDTO){
